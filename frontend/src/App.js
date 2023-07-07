2054aa4b15fd1cb34c7827ab8cf1c6febd706d40
sulfidate <ffluid@me.com>
2023-07-07 09:47:27 +0200

    delete import react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import logo from '../assets/logo.png'
