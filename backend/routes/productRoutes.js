51ee21a58f236ab835ac3dd2e6c8bfeca4e7b27a
sulfidate <ffluid@me.com>
2023-07-14 12:55:31 +0200

    Custom Error Middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import Product from '../models/productModel.js'
 
 router.get('/', asyncHandler ( async (req, res) => {
     const products = await Product.find({})
+    // throw new Error("Some error")
     res.json(products)
     }
 ))
@@ -15,7 +16,9 @@ router.get('/:id', asyncHandler (async (req, res) => {
     res.json(product)
     }
     else {
-        res.status(404).json({message: "Product not found"})
+        res.status(404)
+        throw new Error("Ressource not found")
+
     }
     }
     
