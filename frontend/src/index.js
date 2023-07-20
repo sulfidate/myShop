4b67ac5ac443907bb50a5ca1731647ae8ddd066d
sulfidate <ffluid@me.com>
2023-07-20 22:49:57 +0200

    CartScreen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,10 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider} from 'react-router-dom';
-  import { Provider } from 'react-redux';
-  import store from './store';
+  RouterProvider
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
@@ -15,12 +16,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
     </Route>
   )
 );
@@ -28,7 +31,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-   <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
