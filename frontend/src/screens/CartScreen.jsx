73a72b95f77d772691e020c42304307d1fe2099b
sulfidate <ffluid@me.com>
2023-07-21 10:13:00 +0200

    Remove from Cart

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
 import { FaTrash } from 'react-icons/fa';
 import Message from '../components/Message';
-import { addToCart } from '../slices/cartSlice';
+import { addToCart, removeFromCart } from '../slices/cartSlice';
 
 const CartScreen = () => {
     const dispatch = useDispatch();
@@ -15,6 +15,16 @@ const CartScreen = () => {
     const addToCartHandler = async (product, qty) => {
         dispatch(addToCart({...product, qty}));
     };
+
+    const removeFromCartHandler = async (id) => {
+        dispatch(removeFromCart(id));
+    };
+
+    const checkoutHandler = () => {
+        navigate('/login?redirect=/shipping');
+    };
+
+
 
     return (
         <Row>
@@ -53,7 +63,7 @@ const CartScreen = () => {
                                             <Button
                                                 type='button'
                                                 variant='light'
-                                                onClick={() => { }}
+                                                onClick={() => {removeFromCartHandler(item._id)}}
                                             >
                                                 <FaTrash />
                                             </Button>
@@ -80,7 +90,7 @@ const CartScreen = () => {
                                 type='button'
                                 className='btn-block'
                                 disabled={cartItems.length === 0}
-                                onClick={() => navigate('/login?redirect=shipping')}
+                                onClick={checkoutHandler}
                             >
                                 Proceed To Checkout
                             </Button>
