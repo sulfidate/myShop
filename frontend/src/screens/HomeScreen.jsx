d22fed5a89549f3df189d55e93547a791727b99f
sulfidate <ffluid@me.com>
2023-09-14 18:24:42 +0200

    Paginate Products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Product from '../components/Product'
 import { Row, Col } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import { useGetProductsQuery } from '../slices/productsApiSlice'
 
 const HomeScreen = () => {
-const { data: products, isLoading, error } = useGetProductsQuery()
+
+    const { pageNumber } = useParams()
+
+    const { data, isLoading, error } = useGetProductsQuery({ pageNumber })
+    
   return (
     <>
 
@@ -17,7 +22,7 @@ const { data: products, isLoading, error } = useGetProductsQuery()
     <Message variant='danger'> {error?.data?.message || error.error} </Message>
     ) : (<><h1>Latest Products</h1>
         <Row>
-            {products.map(product => (
+            {data.products.map(product => (
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <Product product={product} />
                 </Col>
